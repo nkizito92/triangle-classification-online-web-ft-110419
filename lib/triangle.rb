0eef8bc0cb16c2e633f31f66ed30d64a7d141fc5
Kizito Njoku <nkizito57@gmail.com>
2019-11-24 00:51:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require "pry"
 class Triangle
-  # write code here
+  
+    def initialize(a,b,c)
+      @a = a 
+      @b = b 
+      @c = c 
+    end 
+    
+    def kind 
+      if (@a <= 0 ) || (@b <= 0) || (@c <= 0 )
+        raise TriangleError
+      elsif (@a + @b <= @c ) || (@a + @c <= @b ) || (@b + @c <= @a )
+        raise TriangleError
+      else 
+        if (@a == @b) && (@b == @c)
+          :equilateral 
+          elsif (@a == @b) || (@b == @c) || (@a == @c)
+          :isosceles 
+          elsif (@a != @b) && (@b != @c) && (@a != @c)
+          :scalene
+        end 
+      end 
+    end 
 end
+
+  class TriangleError < StandardError
+    def message 
+      "Something is not right here"
+    end 
+  end
